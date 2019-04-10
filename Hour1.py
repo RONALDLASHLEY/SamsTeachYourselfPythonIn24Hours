c7aa557861064e293e742261127e5b1bb88684ea
Ronald M. Lashley <lashleyronald1@gmail.com>
2019-04-10 11:03:12 -0400

    Add files via upload

---
+++ Hour1.py
@@ -0,0 +1,23 @@
+"""
+Sams Teach Yourself Python in 24 Hours
+by Katie Cunningham
+
+Hour 1: Installing and Running Python
+Exercise:
+Using Command Line 
+1.
+       a) create new folder called 'projects'
+       b) change into that directory
+       c) create new folder called 'python'
+2.
+       a) create new file 'hello.py' in 'projects/python'
+       b) in 'hello.py' type 'print("Hello, world")'
+       c) save
+       d) display size of file
+
+"""
+
+#Hour 1: Installing and Running Python
+
+print ("Hello, world")
+
